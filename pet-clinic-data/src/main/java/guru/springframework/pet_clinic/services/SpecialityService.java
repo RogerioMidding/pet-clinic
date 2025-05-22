7bc5df49bf91332579e5185e2a26edb8bd81a6c3
RogerioMidding <rogerio_midding@hotmail.com>
2025-05-22 16:08:00 -0300

    added speciality map service. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/pet_clinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package guru.springframework.pet_clinic.services;
+
+import guru.springframework.pet_clinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
