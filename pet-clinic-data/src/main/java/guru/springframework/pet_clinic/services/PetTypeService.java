176348107c7870785b3e3bba9a2d2ac943b721c7
RogerioMidding <rogerio_midding@hotmail.com>
2025-05-22 14:51:10 -0300

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/pet_clinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.pet_clinic.services;
+
+import guru.springframework.pet_clinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
