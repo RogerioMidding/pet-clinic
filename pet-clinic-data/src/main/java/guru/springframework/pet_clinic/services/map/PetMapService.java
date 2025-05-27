1d024ec01120b84d8420f112fab6ca186edc36b4
RogerioMidding <rogerio_midding@hotmail.com>
2025-05-27 14:19:59 -0300

    refactoring class names

--- pet-clinic-data/src/main/java/guru/springframework/pet_clinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/pet_clinic/services/map/PetMapService.java
@@ -8,7 +8,7 @@ import guru.springframework.pet_clinic.model.Pet;
 import guru.springframework.pet_clinic.services.PetService;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
 	@Override
 	public Set<Pet> findAll() {
